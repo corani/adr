57c6a51d05e1fa4b84baaddaa329a75acd55ce6b
Daniel Bos <daniel.bos@sap.com>
2021-12-29 17:45:55 +0800

    `adr show` frontmatter
    closes #14

--- internal/show.go
+++ internal/show.go
@@ -2,9 +2,11 @@ package internal
 
 import (
 	"fmt"
+	"os"
 
 	markdown "github.com/MichaelMure/go-term-markdown"
 	"github.com/corani/adr/internal/adr"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 func Show(id int) error {
@@ -20,8 +22,22 @@ func Show(id int) error {
 
 	for _, v := range list {
 		if v.Number == adr.Number(id) {
-			out := markdown.Render(string(v.Body), 80, 2)
+			t := table.NewWriter()
 
+			t.SetOutputMirror(os.Stdout)
+			t.SetStyle(table.StyleRounded)
+			t.AppendRows([]table.Row{
+				{"Filename", v.Filename},
+				{"Number", fmt.Sprintf("%04d", v.Number)},
+				{"Date", v.Date},
+				{"Status", v.Status},
+			})
+
+			t.Render()
+
+			out := markdown.Render(string(v.Body), 80, 1)
+
+			fmt.Println()
 			fmt.Println(string(out))
 
 			return nil
